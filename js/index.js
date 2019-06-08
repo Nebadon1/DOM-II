baf7829f9b5f60c20ca2f88611560bc31bfb7099
Carlos Mitchell <kakismitchell@gmail.com>
2019-06-07 22:17:38 -0400

    mouseover Keydown and wheel

--- js/index.js
+++ js/index.js
@@ -1,1 +1,38 @@
 // Your code goes here
+
+//MOUSE OVER 
+
+// let introImg = document.querySelectorAll(".nav .nav-link");
+
+let introImg = document.querySelector("nav");
+
+introImg.addEventListener("mouseover", function( event ) {   
+    // highlight the mouseover target
+    event.target.style.color = "orange";
+    event.target.style.fontSize = "1.8rem";
+  });
+
+  //font size change with hover
+  
+  
+//WITH MOUSEOUT 
+
+  introImg.addEventListener("mouseout", function( event ) {   
+    // highlight the mouseover target
+    event.target.style.color = "black";
+    event.target.style.fontSize = "1.6rem";
+  });
+
+ 
+
+
+
+  //WHEEL
+
+  window.addEventListener("wheel", function( event ){
+    var x = document.getElementById("toast")
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 7000);
+
+    
+  });
